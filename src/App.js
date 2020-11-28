59e339087cb331daffe7995142a8d2bf48e354f6
marciio camello <mac3designer@gmail.com>
2020-11-27 23:06:04 -0300

    Change app message

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Deploy S3 React + Codepipeline
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Deploy S3 React + Codepipeline
-        </a>
       </header>
     </div>
   );
